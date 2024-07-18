ba6c8f7f3fd9b8f2e03c96b75c8bc7ddf865d562
ASHISH SAHU <156110323+Ashishfsw@users.noreply.github.com>
2024-07-18 15:30:02 +0530

    Add files via upload

---
+++ trainning/src/com/pack1/ClassA.java
@@ -0,0 +1,16 @@
+
+import java.io.*;
+
+
+class ClassA 
+{
+	
+	public static void main(String[] args) {
+	
+		System.out.println("enter 1st");
+		
+		System.out.println("enter intg") ;
+		
+	}
+
+}
---
+++ trainning/src/com/pack1/ClassB.java
@@ -0,0 +1,18 @@
+package com.pack1;
+
+public class ClassB {
+
+class ClassA 
+{
+	
+	public static void main(String[] args) {
+	
+		System.out.println("enter 1st");
+		
+		System.out.println("enter intg") ;
+		
+	}
+
+}
+
+}
