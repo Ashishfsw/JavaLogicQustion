d67b78a34db61227f5f61664fc44001a17f82a8f
ASHISH SAHU <156110323+Ashishfsw@users.noreply.github.com>
2024-07-18 15:28:38 +0530

    Add files via upload

---
+++ String/src/com/ashish/string/StringMethodPractice.java
@@ -0,0 +1,15 @@
+package com.ashish.string;
+
+public class StringMethodPractice {
+public static void main(String[] args) {
+//	String a = "Nareshit";
+//	String b = "Reshit";
+//	boolean d = a.endsWith("hgsfjst");
+    String z = "hello hlw";
+    int a = z.length();
+    
+   
+	System.out.println(a);
+
+}
+}
